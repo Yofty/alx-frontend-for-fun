c553c2840bd4be21846d773be10298454ce58e89
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-06 03:44:40 -0700

    all tasks

---
+++ markdown2html.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" ascripts that codes markdow to HTML"""
+import sys
+import os
+import re
+
+if __name__ == '__main__':
+    if len(sys.argv[1:]) != 2:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not (os.path.exists(inpute_file) and  os.path.isfile(input_file)):
+        print(f'Missing {input_file}', file=sys.stderr)
+        sys.exit(1)
+
+    with open(input_file, encoding='uft-8') as file_1:
+        html_content = []
+        md_content =[line[:-1] for line in file_1.readlines()]
+        for line in md_content:
+            heading = re.split(r'#{1,6}', line)
+            if len(heading) > 1:
+                h_level = len(line[:line.find(heading[1]) -1])
+                html_content.append(
+                    f'<h{h_level}>{heading[1]]</h{h_level}>\n'
+                    )
+            else:
+                html_content.append(line)
+
+    with open(output_file, 'w', encoding='utf-8') as file_2:
+        file_2.writelines(html_content)
